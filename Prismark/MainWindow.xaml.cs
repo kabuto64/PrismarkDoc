c9b6253dc455ed8ad941f1581a5758f5e2c60c5d
kabuto64 <tky1094@gmail.com>
2024-09-17 01:03:04 +0900

    new Comit

---
+++ Prismark/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Prismark.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prismark
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.StateChanged += MainWindow_StateChanged;
+        }
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.BorderThickness = new Thickness(8);
+                double taskBarHeight = SystemParameters.PrimaryScreenHeight - SystemParameters.MaximizedPrimaryScreenHeight;
+                this.MaxHeight = SystemParameters.PrimaryScreenHeight - taskBarHeight;
+            }
+            else
+            {
+                this.BorderThickness = new Thickness(0);
+            }
+        }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NavigateToEditor(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Resources.Pages.Editor());
+        }
+
+        private void NavigateToSettings(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Resources.Pages.Setting());
+        }
+    }
+}
---
+++ Prismark/Resources/Pages/Editor.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Highlighting;
+using Markdig;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Prismark.Resources.Pages
+{
+    /// <summary>
+    /// Editor.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Editor : Page
+    {
+        public Editor()
+        {
+            InitializeComponent();
+            InitializeAsync();
+            // シンタックスハイライト定義ファイルを読み込む
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream s = assembly.GetManifestResourceStream("Prismark.Resources.Themes.MarkdownDark.xshd"))
+            {
+                using (XmlReader reader = new XmlTextReader(s))
+                {
+                    MarkDownEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+            
+            // テキスト変更時のイベントハンドラを設定
+            MarkDownEditor.TextChanged += MarkDownEditor_TextChanged;
+
+        }
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+
+            string htmlContent = @"
+            <html>
+            <head>
+                <style>
+                    body { 
+                        font-family: Arial, sans-serif;
+                        background-color: #222;
+                        color: #c4c4c4;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Hello, World!</h1>
+                <p>This is a paragraph.</p>
+            </body>
+            </html>";
+
+            webView.NavigateToString(htmlContent);
+        }
+        private void ToggleMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (LeftColumn.Width.Value > 50)
+            {
+                LeftColumn.Width = new GridLength(50);
+            }
+            else
+            {
+                LeftColumn.Width = new GridLength(200);
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MarkDownEditor_TextChanged(object sender, EventArgs e)
+        {
+            string markdown = MarkDownEditor.Text;
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            string html = Markdown.ToHtml(markdown, pipeline);
+            webView.NavigateToString(html);
+        }
+    }
+   
+}
