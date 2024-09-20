f2da3151538c836153436ec6a3180684ea7c8f95
kabuto64 <tky1094@gmail.com>
2024-09-20 20:41:25 +0900

    Commit

--- Prismark/Resources/Pages/StartUp.xaml.cs
+++ Prismark/Resources/Pages/StartUp.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IWshRuntimeLibrary;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Prismark.Resources.Pages
@@ -24,6 +27,7 @@ namespace Prismark.Resources.Pages
     {
         public string WorkingFolder { get; private set; }
         public string ProjectName { get; private set; }
+        public string ProjectPath { get; private set; }
         public StartUp()
         {
             InitializeComponent();
@@ -43,5 +47,59 @@ namespace Prismark.Resources.Pages
                 txtFolderPath.Text = dialog.FileName;
             }
         }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFolderPath.Text))
+            {
+                System.Windows.MessageBox.Show("作業フォルダを選択してください。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtProjectName.Text))
+            {
+                System.Windows.MessageBox.Show("ドキュメント名を入力してください。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            WorkingFolder = txtFolderPath.Text;
+            ProjectName = txtProjectName.Text;
+
+            ProjectPath = System.IO.Path.Combine(WorkingFolder, ProjectName);
+
+            CreateRequiredFolders();
+            CreateShortcut();
+
+            try
+            {
+                Directory.CreateDirectory(ProjectPath);
+                System.Windows.MessageBox.Show($"プロジェクトフォルダの作成が完了しました", "完了", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"プロジェクトフォルダの作成中にエラーが発生しました: {ex.Message}", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void CreateRequiredFolders()
+        {
+            Directory.CreateDirectory(System.IO.Path.Combine(ProjectPath, "md"));
+            Directory.CreateDirectory(System.IO.Path.Combine(ProjectPath, "img"));
+        }
+
+        private void CreateShortcut()
+        {
+            string shortcutPath = System.IO.Path.Combine(ProjectPath, $"{ProjectName}.lnk");
+            string targetPath = Process.GetCurrentProcess().MainModule.FileName;
+
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+
+            shortcut.TargetPath = targetPath;
+            shortcut.Arguments = $"--project \"{ProjectPath}\"";
+            shortcut.WorkingDirectory = ProjectPath;
+            shortcut.Description = $"{ProjectName} - DocuForge Project";
+            shortcut.Save();
+        }
+
     }
 }
